1716ce8f1aeb358c0e1f7bb6a07e2485b4c305c4
Dimitris Zacharatos <sdi2400048@di.uoa.gr>
2024-11-24 17:20:49 +0200

    Working on it

--- rsa/src/rsa.c
+++ rsa/src/rsa.c
@@ -1,39 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 int prime_check(long long int number);
+long long int gcd(long long int e, long long int phiN);
 
 int main(int argc, char * argv[]){
-
+    long long int N,message,phip,phiq,m,phiN;
+    // check for correct amount of arguments 
     if (argc!=6){
         printf("Usage: ./rsa enc|dec <exp_exp> <priv_exp> <prime1> <prime2>\n");
         return 1;
     }
-    char c = argv[1][2];                            
-    if ( argv[1] != "dec" || argv[1] !="enc"){
+    // check for correct syntax of the first argument
+    char op = argv[1][0];         // changes the first argument into a veriable to be able to compere it with the correct form ('dec' or 'enc')               
+    if ( op != 'd' && op !='e'){
         printf("First argument must be 'enc' or 'dec'\n");
         return 1;
     }
+    // chenges all of the rest arguments into numbers 
     long long int e = atoll(argv[2]);
     long long int d = atoll(argv[3]);
     long long int p = atoll(argv[4]);
     long long int q = atoll(argv[5]);
+    // checks for any negative numbers
     if (e<0 || d<0 || p<0 || q<0){
         printf("Negative numbers are not allowed\n");
-    }
-    if (prime_check(p)!=2 || prime_check(q)!=2){
-        printf("p and q must be prime\n");
         return 1;
     }
+    // checks if p and q are prime 
+    if (prime_check(p)!=2 || prime_check(q)!=2){ // uses the function prime_check that returns how many times the number can be divited and because prime   
+        printf("p and q must be prime\n");      // numbers can only be divided with them selfes and 1 if the number that is reterund is not 2 then its not prime
+        return 1;
+    }
+    // because p and q are prime by using the totient function we find the coprime of p and q
+    phip = p-1;
+    phiq = q-1;
+    phiN = phip * phiq;     // phiN is the amount of coprimes of p and q
+    // checks if e is not coprime with phi(N)
+    if (gcd(e,phiN)!=1){             // using the function gcd if the gcd is 1 then e is coprime with phiN 
+        printf("e is not coprime with phi(N)\n");
+        return 1;
+    }
+    // checks if e and d are reversable 
+    if (e * d % phiN != 1){                 // this (e * d % phiN) is the way to see if e and d are reversable
+        printf("e * d mod phi(N) is not 1\n");
+        return 1;
+    }
+    N = p*q;
+    message = scanf("%lld", &m);     //reads the message that will be encupted or decrupted 
+    // checks if only 1 number was read      
+    if (message!=1){
+        printf("Usage: ./rsa enc|dec <exp_exp> <priv_exp> <prime1> <prime2>\n");
+        return 1;
+    }
+    // ckecks if the massege is a negative number
+    if (m<0){
+        printf("Negative numbers are not allowed\n");
+        return 1;
+    }
+    // ckecks if message is larger than N
+    if (m>= N){
+        printf("Message is larger than N\n");
+        return 1;
+    }
+
     return 0;
 }
 
 
-int prime_check(long long int number){
-    long long int times=0;
-    for(int i=1;i<=number;i++){
+int prime_check(long long int number){        
+    long long int times=0;          // makes the counter start from 0
+    // divides all of the numbers that are equal or lower from the main number
+    for(int i=1;i<=number;i++){ 
+        // adds 1 to the counter every time it finds a number that can be divided with it
         if(number % i == 0){
             times++;
         }
     }
-    return times;
+    return times;       // returns the amount of numbers that are dividable with it
+}
+
+
+
+long long int gcd(long long int e, long long int phiN){ 
+    // finds with the help of the Euclidean Algorithm the GCD 
+    if (e % phiN == 0){
+        return phiN;            // returns the correct answer
+    } else {
+        return gcd(phiN,e % phiN);       // calls again the same funcion with diffrent argumets until it finds the GCD
+    }
 }
