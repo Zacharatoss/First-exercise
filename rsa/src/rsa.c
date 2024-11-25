2448b285794612e14a64566aa38a19eeba12f78e
Dimitris Zacharatos <sdi2400048@di.uoa.gr>
2024-11-25 09:51:43 +0200

    almost

--- rsa/src/rsa.c
+++ rsa/src/rsa.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 int prime_check(long long int number);
 long long int gcd(long long int e, long long int phiN);
-long long int decrypt(long long int m,long long int d,long long int N);
-long long int encrypt(long long int m,long long int e,long long int N);
+long long int module(long long int m,long long int exp,long long int N);
 
 int main(int argc, char * argv[]){
     long long int N,message,phip,phiq,m,phiN;
@@ -65,9 +64,9 @@ int main(int argc, char * argv[]){
         return 1;
     }
     if ( op == 'e'){
-        encrypt(m,e,N);
+        module(m,e,N);
     } else {
-        decrypt(m,d,N);
+        module(m,d,N);
     }
 }
 
@@ -93,22 +92,15 @@ long long int gcd(long long int e, long long int phiN){
     }
 }
 
-long long int decrypt(long long int m,long long int d,long long int N){
+long long int module(long long int m,long long int exp,long long int N){
     long long int result;
-    result= m^d % N;
-    printf("%lld\n", result); 
-    return 0;
-}
-
-long long int encrypt(long long int m,long long int e,long long int N){
-    long long int result;
-    unsigned long long power;
-    power=1;
-    for (long long int i=0;i<e;i++){
-        printf("%lld\n", power);
-        power *= m;
+    if (N==1){
+        return 0;
     }
-    result = power % N;
+    result = 1;
+    for (long long int i=1;i<=exp;i++){
+        result = (result * m) % N;
+    }
     printf("%lld\n", result);
     return 0;
 }
