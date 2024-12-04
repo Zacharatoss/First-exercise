1b0b8967ca3aa3cb7bc996d5d9f5e354b9bfabfb
Dimitris Zacharatos <sdi2400048@di.uoa.gr>
2024-12-04 15:40:09 +0200

    Started

---
+++ factor/src/factor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    printf("Usage: ./factor <semiprime>0\n");
+    return 1;
+  }
+long long sem = atoll(argv[1]);
+
+return 0; 
+
+}
