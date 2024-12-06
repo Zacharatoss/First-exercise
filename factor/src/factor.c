70c19cec0e96bf59c32bc2b78c58cae1b437d2fe
Dimitris Zacharatos <sdi2400048@di.uoa.gr>
2024-12-06 17:50:45 +0200

    not working for level extreme

--- factor/src/factor.c
+++ factor/src/factor.c
@@ -3,12 +3,19 @@
 #include <math.h>
 
 int main(int argc, char* argv[]) {
+  long long int num1=0,num2,i;
   if (argc != 2) {
-    printf("Usage: ./factor <semiprime>0\n");
+    printf("Usage: ./factor <semiprime>\n");
     return 1;
   }
 long long sem = atoll(argv[1]);
-
-return 0; 
-
+for (i=2;i<=sqrt(sem);i++){
+    if (sem % i ==0){
+      num1 = i;
+      break;
+    }
+}
+num2 = sem/num1;
+printf("Factors: %lld %lld\n", num1,num2);
+return 0;
 }
