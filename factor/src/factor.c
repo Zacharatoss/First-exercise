39e04728ddd5930babd91c292330d88dc7e00d59
Dimitris Zacharatos <sdi2400048@di.uoa.gr>
2024-12-16 19:29:37 +0200

    Working for very hard

--- factor/src/factor.c
+++ factor/src/factor.c
@@ -1,21 +1,28 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 int main(int argc, char* argv[]) {
-  long long int num1=0,num2,i;
+  long long int num1 = 0, num2, i;
+  // check for correct amount of arguments
   if (argc != 2) {
-    printf("Usage: ./factor <semiprime>\n");
+    printf("Usage: %s <semiprime>\n", argv[0]);
     return 1;
   }
-long long sem = atoll(argv[1]);
-for (i=2;i<=sqrt(sem);i++){
-    if (sem % i ==0){
-      num1 = i;
+  long long int sem = atoll(argv[1]);  // chenges the argument into a number
+  // by using the square root function we minimize the numbers that will be
+  // checked
+  for (i = 2; i <= sqrt(sem); i++) {
+    if (sem % i ==
+        0) {     // if it finds a number that can divide sem then the loop ends
+      num1 = i;  // stores the number that can divide our number in the veriable
+                 // num1
       break;
     }
-}
-num2 = sem/num1;
-printf("Factors: %lld %lld\n", num1,num2);
-return 0;
+  }
+  num2 = sem / num1;  // dividing our starting number with num1 with find the
+                      // second number that can divide our number
+  printf("Factors: %lld %lld\n", num1,
+         num2);  // prints the two numbers that are the factors
+  return 0;
 }
